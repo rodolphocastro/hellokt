3c79d77f795335c0f32af29440a9a784a19bbad5
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-01-29 21:16:46 -0300

    test: Test out lambdas

--- src/test/kotlin/Functional.kt
+++ src/test/kotlin/Functional.kt
@@ -5,6 +5,7 @@
 @file:Suppress("ClassName")
 
 import org.junit.Test
+import kotlin.math.exp
 import kotlin.test.assertEquals
 
 /**
@@ -34,6 +35,10 @@ class `Functional Features in Kotlin` {
         return result
     }
 
+    /**
+     * Since we can reference and store functions Kotlin allows us to use higher-order functions.
+     * Aka: Passing and returning functions just works. For the C# nerds: This means we can use Action and Func on Kt ;)
+     */
     @Test
     fun `Functions can be used as parameters and returns for other functions`() {
         // Arrange
@@ -73,5 +78,41 @@ class `Functional Features in Kotlin` {
         assertEquals(expectedBase * 2, got)
         assertEquals(expectedBase * 3, gotTriple)
         assertEquals(5 * 4, anotherGot())   // Lazily invoking anotherGot
+    }
+
+    /**
+     * We already used some in the previous tests but hey! Time for a specific explanation for Lambdas :)
+     */
+    @Suppress("LocalVariableName")
+    @Test
+    fun `Lambdas (aka ad-hoc functions) are also available in Kotlin, with and without type inference`(): Unit {
+        // Arrange
+        val base = 2
+        val expected = base * 4
+
+        val `a Lambda without ANY type inference`: (Int) -> Int =
+            { subject: Int -> subject * 4 }  // Quite verbose, right?
+        val `a Lambda with type inference on the inside`: (Int) -> Int =
+            { subject -> subject * 4 } // A bit tidier... but we can do better
+        val `a Lambda with type inference on the outside` = { subject: Int ->
+            subject * 4
+        } // A lot better!
+        // val aLambdaThatWontCompile = { subject -> subject * 4 }
+        // The line above won't compile because there's not enough information for types to be inferred
+        val `a Lambda with a single parameter can use the it keyword inside`: (Int) -> Int = {
+            it * 4
+        }  // As long as there's only one parameter you can use the "it" variable instead of naming stuff just for lulz
+
+        // Act
+        val firstGot = `a Lambda without ANY type inference`(base)
+        val secondGot = `a Lambda with type inference on the inside`(base)
+        val thirdGot = `a Lambda with type inference on the outside`(base)
+        val fourthGot = `a Lambda with a single parameter can use the it keyword inside`(base)  // The cleanest one imho
+
+        // Assert
+        assertEquals(expected, firstGot)
+        assertEquals(expected, secondGot)
+        assertEquals(expected, thirdGot)
+        assertEquals(expected, fourthGot)
     }
 }
